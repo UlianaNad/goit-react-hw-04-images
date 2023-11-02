58954bcad7645ae176deeb81933b6e0687ab6d87
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-11-02 15:53:01 +0200

    -striktmode

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,59 +5,58 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
-import {Modal} from './Modal/Modal';
+import { Modal } from './Modal/Modal';
 import Loader from './Loader/Loader';
 
 export const App = () => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [setError] = useState(null);
   const [page, setPage] = useState(1);
   const [photos, setPhotos] = useState([]);
-  const [per_page, setPer_page] = useState(12);
+  const [per_page] = useState(12);
   const [q, setQ] = useState('');
   const [total, setTotal] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const [imgInfo, setImgInfo] = useState(null);
 
-  useEffect(()=>{
-    if(q){
-      getPhotos({ page, per_page, q: q, fn: fetchPhotosByQuery })
-    } else{
-      getPhotos({ page, per_page, fn: fetchPhotos })
+  useEffect(() => {
+    const getPhotos = async ({ page, per_page, q, fn }) => {
+      try {
+        setLoading(true);
+        const { hits, total } = await fn({
+          q: q,
+          per_page: per_page,
+          page: page,
+        });
+        setPhotos(prev => [...prev, ...hits]);
+        setTotal(total);
+      } catch (error) {
+        setError(error.message);
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (q) {
+      getPhotos({ page, per_page, q: q, fn: fetchPhotosByQuery });
+    } else {
+      getPhotos({ page, per_page, fn: fetchPhotos });
     }
-  },[page, per_page, q])
-
-  const getPhotos = async ({ page, per_page, q, fn }) => {
-    try {
-      setLoading(true);
-      const { hits, total } = await fn({
-        q: q,
-        per_page: per_page,
-        page: page,
-      });
-      setPhotos(prev => [...prev, ...hits]);
-      setTotal(total);
-    } catch (error) {
-      setError(error.message);
-      toast.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  }, [page, per_page, q, setError]);
 
   const handleQuery = q => {
-    setQ(q)
-    setPhotos([])
-    setPage(1)
+    setQ(q);
+    setPhotos([]);
+    setPage(1);
   };
 
   const toggleModal = imgInfo => {
-    setIsOpen(prev => !prev)
-    setImgInfo(imgInfo)
+    setIsOpen(prev => !prev);
+    setImgInfo(imgInfo);
   };
 
   const handleLoadMore = () => {
-    setPage(prev => prev + 1)
+    setPage(prev => prev + 1);
   };
 
   return (
@@ -79,7 +78,9 @@ export const App = () => {
           {loading ? 'Loading...' : 'Load more'}
         </Button>
       ) : null}
-      {isOpen ? <Modal close={toggleModal} imgInfo={imgInfo} isOpen={isOpen}></Modal> : null}
+      {isOpen ? (
+        <Modal close={toggleModal} imgInfo={imgInfo} isOpen={isOpen}></Modal>
+      ) : null}
       <ToastContainer />
     </div>
   );
